8bfab664207578b8acdf4e852bae5081f9907d69
Thibaut Cuvelier <dourouc05@gmail.com>
2017-01-30 13:46:07 +0100

    Move ideas to a TODO file.

--- src/ReservoirManagement.jl
+++ src/ReservoirManagement.jl
@@ -1,5 +1,44 @@
 module ReservoirManagement
 
-# package code goes here
+  using TimeSeries
+  using Base.Dates
+  using DataStructures
+  using SIUnits
+  using SIUnits.ShortUnits
+  
+  using JuMP
+  using Gurobi
+  
+  using StatsBase
+  using HypothesisTests
+  using Distributions
+  using ExtremeValueDistributions # Strange dependency (not published in METADATA), try to reimplement somewhere
+  # (and make it fully compatible with Julia 0.4, without warnings).
+  
+  using DataFrames
+  using Colors
+  using Plots
+  using StatPlots
+  
+  # The generalised distributions are defined in both Distributions and ExtremeValueDistributions, but those are identical.
+  GeneralizedExtremeValue = Distributions.GeneralizedExtremeValue
+  GeneralizedPareto = Distributions.GeneralizedPareto
+  
+  # Conflict between standard Dates and SIUnits.
+  Second = Dates.Second
+  
+  # To allow overriding those functions.
+  import Base: copy, isempty, start, next, done, eltype, length
+  
+  # Order of inclusion is important!
+  include("utils.jl")
+  include("reservoir/_ds.jl")
+  include("read.jl")
+  include("stats/_stats.jl")
+  include("scenariosgeneration.jl")
+  include("solvers/_solvers.jl")
+  include("plots_dataframe.jl")
+  include("plots_plots.jl")
+  include("report.jl")
 
-end # module
+end 
